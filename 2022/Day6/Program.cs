d0228f16dbcad5fa7f6df2390575463c3f632445
McjMzn <maz.an@interia.pl>
2022-12-06 19:43:06 +0100

    added day 5

---
+++ 2022/Day6/Program.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode;
+using System;
+using System.Linq;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Input.Load();
+
+            // Part 1
+            Console.WriteLine($"Part 1: {FindEndOfMarker(input, 4)}");
+
+            // Part 2
+            Console.WriteLine($"Part 2: {FindEndOfMarker(input, 14)}");
+        }
+
+        private static int FindEndOfMarker(string input, int markerLength)
+        {
+            for (var i = 0; i < input.Length - markerLength; i++)
+            {
+                if (input.Substring(i, markerLength).ToHashSet().Count == markerLength)
+                {
+                    return i + markerLength;
+                }
+            }
+
+            throw new Exception("Marker not found.");
+        }
+    }
+}
--- AdventOfCode/Input.cs
+++ AdventOfCode/Input.cs
@@ -9,13 +9,18 @@ namespace AdventOfCode
 {
     public class Input
     {
-        public static void Process(Action<string> onLineRead)
+        public static void Process(Action<string> onLineRead, string filePath = "input.txt")
         {
-            var lines = File.ReadAllLines("input.txt");
+            var lines = File.ReadAllLines(filePath);
             foreach(var line in lines)
             {
                 onLineRead(line);
             }
+        }
+
+        public static string Load(string filePath = "input.txt")
+        {
+            return File.ReadAllText(filePath);
         }
     }
 }
