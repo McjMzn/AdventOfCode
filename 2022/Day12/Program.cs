4170c69f124c3bbe2f818bef263b31500dee5608
McjMzn <maz.an@interia.pl>
2022-12-12 22:42:37 +0100

    added day 12

---
+++ 2022/Day12/Program.cs
@@ -0,0 +1,115 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class Terrain
+{
+    public Terrain(char c)
+    {
+        switch (c)
+        {
+            case 'S': this.IsStart = true; this.Height = 1; break;
+            case 'E': this.IsEnd = true; this.Height = 'z' - 'a' + 1; break;
+            default: this.Height = c - 'a' + 1; break;
+        }
+    }
+
+    public int Height { get; set; }
+    public bool IsStart { get; set; }
+    public bool IsEnd { get; set; }
+
+    public int TravelCost { get; set; } = int.MaxValue;
+    public bool IsVisited { get; set; } = false;
+
+    public override string ToString()
+    {
+        return $"{this.TravelCost,2} ";
+    }
+}
+
+internal class Program
+{
+    private static Grid<Terrain> LoadMap(string input)
+    {
+        var map = new Grid<Terrain>();
+        var lines = input.Split(Environment.NewLine);
+        map.Height = lines.Count();
+        map.Width = lines[0].Count();
+
+        map.Nodes = lines.ToList().SelectMany(x => x).Select(c => new Terrain(c)).ToList();
+
+        map.Nodes.Single(n => n.IsStart).TravelCost = 0;
+
+        return map;
+    }
+
+    private static int FindShortestPath(Grid<Terrain> map)
+    {
+        var neighbours =
+            map
+            .Nodes
+            .ToDictionary(
+                node => node,
+                node => map.GetNeighbours(node)
+            );
+
+        var endNode = map.Nodes.Single(n => n.IsEnd);
+        map.Nodes.Single(n => n.IsStart).TravelCost = 0;
+
+        while (!endNode.IsVisited)
+        {
+            var unvisited = map.Nodes.Where(n => !n.IsVisited);
+            var current = unvisited.Where(n => n.TravelCost != int.MaxValue).OrderBy(n => n.TravelCost).FirstOrDefault();
+            if (current is null)
+            {
+                return int.MaxValue;
+            }
+
+            foreach(var neighbour in neighbours[current])
+            {
+                if (neighbour.Height <= current.Height || current.Height + 1== neighbour.Height)
+                {
+                    neighbour.TravelCost = neighbour.TravelCost == int.MaxValue || current.TravelCost + 1 < neighbour.TravelCost ? current.TravelCost + 1 : neighbour.TravelCost;
+                }
+            }
+
+            current.IsVisited = true;
+        }
+
+        return endNode.TravelCost;
+    }
+
+    private static void Main(string[] args)
+    {
+        // Demo
+        var demoInput = Input.Load("demo_input.txt");
+        var demoMap = LoadMap(demoInput);
+        var demoResult = FindShortestPath(demoMap);
+        Console.WriteLine($"Demo: {demoResult} (should be 31).");
+
+        // Part 1
+        var part1Input = Input.Load();
+        var part1Map = LoadMap(part1Input);
+        var part1Result = FindShortestPath(part1Map);
+        Console.WriteLine($"Part 1: {part1Result}");
+
+        // Part 2
+        var part2Input = Input.Load().Replace("S", "a");
+        var indices = Regex.Matches(part2Input, "a").ToList().Select(m => m.Index).ToList();
+        var results = new List<int>();
+        Parallel.ForEach(indices, new ParallelOptions { MaxDegreeOfParallelism = 8 }, index =>
+        {
+            var input = new StringBuilder(part2Input);
+            input[index] = 'S';
+            var map = LoadMap(input.ToString());
+            var result = FindShortestPath(map);
+            results.Add(result);
+        });
+
+        Console.WriteLine($"Part 2: {results.Where(a => a > 0).OrderBy(a => a).First()}");
+    }
+}
---
+++ AdventOfCode/Grid.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class Grid<T> where T : class
+    {
+        public List<T> Nodes { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public T GetLeftFrom(T node)
+        {
+            (var y, var x) = this.GetIndices(node);
+            return x == 0 ? null : this.Get(y, x - 1);
+        }
+
+        public T GetRightFrom(T node)
+        {
+            (var y, var x) = this.GetIndices(node);
+            return x == this.Width - 1 ? null : this.Get(y, x + 1);
+        }
+
+        public T GetDownFrom(T node)
+        {
+            (var y, var x) = this.GetIndices(node);
+            return y == 0 ? null : this.Get(y - 1, x);
+        }
+
+        public T GetUpFrom(T node)
+        {
+            (var y, var x) = this.GetIndices(node);
+            return y == this.Height - 1 ? null : this.Get(y + 1, x);
+        }
+
+        public List<T> GetNeighbours(T node)
+        {
+            (var y, var x) = this.GetIndices(node);
+
+            return
+                new List<T>
+                {
+                    this.GetLeftFrom(node),
+                    this.GetRightFrom(node),
+                    this.GetUpFrom(node),
+                    this.GetDownFrom(node),
+                }
+                .Where(n => n is not null)
+                .ToList();
+        }
+
+        public T Get(int y, int x)
+        {
+            return this.Nodes[y * this.Width + x];
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            for (var i = 1; i <= this.Nodes.Count; i++)
+            {
+                builder.Append(this.Nodes[i - 1].ToString());
+                if (i % this.Width == 0)
+                {
+                    builder.Append(Environment.NewLine);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private (int Y, int X) GetIndices(T node)
+        {
+            var index = this.Nodes.IndexOf(node);
+
+            var y = index / this.Width;
+            var x = index % this.Width;
+
+            return (y, x);
+        }
+    }
+}
