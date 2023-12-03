40baf590c8fa7a62ad53c44413790404cb399867
McjMzn <maz.an@interia.pl>
2023-12-03 11:45:32 +0100

    add day3 template

---
+++ 2023/Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demo should give 4361
+            Input.ProcessDemo(line =>
+            {
+
+            });
+        }
+    }
+}
